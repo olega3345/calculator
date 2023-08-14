7fe2bd2368a7999f1462822226cebc3fbdddd07d
Олег <olegcelysev778@gmail.com>
2023-08-14 23:08:33 +0300

    калькулятор

--- src/main/java/pro/sky/skyprospringdemo/CalController.java
+++ src/main/java/pro/sky/skyprospringdemo/CalController.java
@@ -1,24 +1,44 @@
 package pro.sky.skyprospringdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    private final HelloService helloService;
-    public HelloController(HelloService helloService) {
-        this.helloService = helloService;
-    }
+@RequestMapping
+public class CalController {
+    private final CalService service;
 
+
+    public CalController(CalService service) {
+        this.service=service;
+    }
     @GetMapping
-    public String hello() {
-        return helloService.hello();
+    public String calculator() {
+        return service.calculator();
     }
 
-    @GetMapping(path = "/hello")
-    public String answerhello(@RequestParam("name") String userName) {
-        return helloService.answerhello(userName);
+    @GetMapping (path= "/calculator/plus")
+    public String plusCalculator (@RequestParam (value = "num1", required = false) Integer num1,
+                                  @RequestParam (value = "num2", required = false) Integer num2)
+    {
+        return service.plusCalculator(num1, num2);
+    }
+    @GetMapping (path= "/calculator/minus")
+    public String minusCalculator (@RequestParam (value = "num1", required = false) Integer num1,
+                                   @RequestParam (value = "num2", required = false) Integer num2){
 
+        return service.minusCalculator(num1, num2);
+    }
+    @GetMapping (path= "/calculator/multiply")
+    public String multiplyCalculator (@RequestParam (value = "num1", required = false) Integer num1,
+                                      @RequestParam (value = "num2", required = false) Integer num2) {
+        return service.multiplyCalculator(num1, num2);
+    }
+    @GetMapping (path= "/calculator/divide")
+    public String divideCalculator (@RequestParam (value = "num1", required = false) Integer num1,
+                                    @RequestParam (value = "num2", required = false) Integer num2) {
+        return service.divideCalculator(num1, num2);
     }
 }
--- src/main/java/pro/sky/skyprospringdemo/SkyproSpringCalculatorApplication.java
+++ src/main/java/pro/sky/skyprospringdemo/SkyproSpringCalculatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkyproSpringDemoApplication {
+public class SkyproSpringCalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkyproSpringDemoApplication.class, args);
+		SpringApplication.run(SkyproSpringCalculatorApplication.class, args);
 	}
 
 }
